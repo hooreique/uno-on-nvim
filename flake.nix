92675f90c791340db1fa8a9a37d952b610034b1f
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2026-08-23 11:50:26 +0900

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,25 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    uno-ls = {
-      url = "github:hooreique/unocss-language-server";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    uno-ls.url = "github:hooreique/unocss-language-server";
   };
 
-  outputs = { self, nixpkgs, flake-utils, uno-ls }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system} // {
-        unocss-language-server = uno-ls.packages.${system}.default;
-      };
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nodejs_22 pnpm typescript-language-server
-          unocss-language-server
-        ];
-      };
-    });
+  outputs =
+    { nixpkgs, uno-ls, ... }:
+    let
+      forAllSys =
+        perSys:
+        nixpkgs.lib.genAttrs [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ] (
+          system:
+          perSys {
+            pkgs = nixpkgs.legacyPackages.${system};
+            unocss-language-server = uno-ls.packages.${system}.default;
+          }
+        );
+    in
+    {
+      devShells = forAllSys (
+        { pkgs, unocss-language-server }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_22
+              pkgs.pnpm
+              pkgs.typescript-language-server
+              unocss-language-server
+            ];
+          };
+        }
+      );
+    };
 }
